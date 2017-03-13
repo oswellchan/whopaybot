7f7753c4c43cd55baf4e857c8d0bb9c787fe9726
Oswell <oswellchan@gmail.com>
2017-03-14 01:15:35 +0800

    Implement adding bill name

--- database.py
+++ database.py
@@ -1,4 +1,8 @@
 import psycopg2
+import uuid
+
+
+UNIQUE_VIOLATION = '23505'
 
 
 class Database:
@@ -49,16 +53,12 @@ class Transaction:
         try:
             self.cursor.execute("""\
                 INSERT INTO users (id, first_name, last_name, username)
-                    VALUES({}, '{}', '{}', '{}')
+                    VALUES(%s, %s, %s, %s)
                 ON CONFLICT(id) DO UPDATE SET
                     id=EXCLUDED.id, first_name=EXCLUDED.first_name,
                     last_name=EXCLUDED.last_name, username=EXCLUDED.username;
-            """.format(
-                user_id,
-                first_name,
-                last_name,
-                username
-            ))
+            """, (user_id, first_name, last_name, username)
+            )
         except Exception as e:
             self.is_error = True
             raise e
@@ -67,24 +67,27 @@ class Transaction:
         try:
             self.cursor.execute("""\
                 INSERT INTO sessions (chat_id, user_id, action, updated_at)
-                    VALUES({}, {}, {}, NOW())
+                    VALUES(%s, %s, %s, NOW())
                 ON CONFLICT(chat_id) DO UPDATE SET
                     chat_id=EXCLUDED.chat_id, user_id=EXCLUDED.user_id,
                     action=EXCLUDED.action, updated_at=EXCLUDED.updated_at;
-            """.format(chat_id, user_id, action)
+            """, (chat_id, user_id, action)
             )
         except Exception as e:
             self.is_error = True
             raise e
 
     def get_pending_action(self, chat_id, user_id):
+        """\
+        Get pending action. Also serves as lock against concurrent.
+        """
         try:
             self.cursor.execute("""\
                 SELECT s.action FROM sessions s
-                WHERE s.chat_id = {}
-                    AND s.user_id = {}
+                WHERE s.chat_id = %s
+                    AND s.user_id = %s
                 FOR UPDATE;
-            """.format(chat_id, user_id)
+            """, (chat_id, user_id)
             )
 
             rows = self.cursor.fetchall()
@@ -95,3 +98,44 @@ class Transaction:
         except Exception as e:
             self.is_error = True
             raise e
+
+    def create_new_bill(self, title, owner_id):
+        try:
+            count = 0
+            while count < 10:
+                bill_id = self.generate_id(16)
+                self.cursor.execute("""\
+                    INSERT INTO bills (id, title, owner_id)
+                        VALUES (%s, %s, %s)
+                    ON CONFLICT(id) DO NOTHING;
+                """, (bill_id, title, owner_id)
+                )
+
+                if self.cursor.rowcount > 0:
+                    return bill_id
+
+                count += 1
+
+            if count >= 10:
+                raise Exception("Collision of bills id")
+        except Exception as ex:
+            self.is_error = True
+            raise ex
+
+    def reset_action(self, chat_id, user_id):
+        try:
+            self.cursor.execute("""\
+                UPDATE sessions
+                    SET action = NULL
+                WHERE chat_id = %s
+                    AND user_id = %s
+            """, (chat_id, user_id)
+            )
+        except Exception as e:
+            self.is_error = True
+            raise e
+
+    @staticmethod
+    def generate_id(length):
+        guid = str(uuid.uuid1())
+        return guid.replace('-', '')[:length]
--- telegrambot.py
+++ telegrambot.py
@@ -3,31 +3,36 @@ from database import Transaction
 import traceback
 
 
+PRIVATE_CHAT = 'private'
 ACTION_NEWBILL_SET_NAME = 0
-TEXT_REQUEST_BILL_NAME = "Send me a name for the new you want to create."
+REQUEST_BILL_NAME = "Send me a name for the new bill you want to create."
+ERROR_INVALID_BILL_NAME = "Sorry, the bill name provided is invalid. Name of the bill can only be 250 characters long."
+ERROR_SOMETHING_WENT_WRONG = "Sorry, an error has occurred. Please try again in a few moments."
 
 
 class TelegramBot:
     def __init__(self, token, db):
         self.db = db
         self.updater = Updater(token=token)
-        self.attach_handlers(self.updater.dispatcher)
+        self.init_handlers(self.updater.dispatcher)
 
     def start_bot(self):
         self.updater.start_polling()
 
-    def attach_handlers(self, dispatcher):
+    def init_handlers(self, dispatcher):
+        # Command handlers
         start_handler = CommandHandler('start', self.start)
         dispatcher.add_handler(start_handler)
         newbill_handler = CommandHandler('newbill', self.new_bill)
         dispatcher.add_handler(newbill_handler)
+
+        # Handle all replies
         message_handler = MessageHandler(Filters.all, self.handle_all)
         dispatcher.add_handler(message_handler)
 
     def start(self, bot, update):
         # TODO: make command list screen
         bot.sendMessage(chat_id=update.message.chat_id, text="Start screen")
-        pass
 
     def new_bill(self, bot, update):
         # only allow private message
@@ -41,14 +46,14 @@ class TelegramBot:
                 )
             bot.sendMessage(
                 chat_id=update.message.chat_id,
-                text=TEXT_REQUEST_BILL_NAME
+                text=REQUEST_BILL_NAME
             )
         except Exception as e:
             traceback.print_trace()
 
     def handle_all(self, bot, update):
         try:
-            if update.message.chat.type != 'private':
+            if update.message.chat.type != PRIVATE_CHAT:
                 return
 
             conn = self.db.get_connection()
@@ -60,9 +65,8 @@ class TelegramBot:
                         msg.chat_id
                     )
                     if pending_action == ACTION_NEWBILL_SET_NAME:
-                        return self.add_bill_name(msg)
+                        return self.add_bill_name(msg, bot, trans)
                 except Exception as e:
-                    print(e)
                     traceback.print_trace()
         except:
             traceback.print_trace()
@@ -77,8 +81,33 @@ class TelegramBot:
         )
         trans.add_session(message.chat_id, user.id, action_type)
 
-    def add_bill_name(self, message):
-        print(Filters.text.filter(message))
-        print(Filters.audio.filter(message))
-        print(Filters.sticker.filter(message))
-        print(Filters.photo.filter(message))
+    def add_bill_name(self, msg, bot, trans):
+        try:
+            if not Filters.text.filter(msg):
+                return bot.sendMessage(
+                    chat_id=msg.chat_id,
+                    text=ERROR_INVALID_BILL_NAME
+                )
+
+            text = msg.text
+            if (text is None or len(text) < 1 or len(text) > 250):
+                return bot.sendMessage(
+                    chat_id=msg.chat_id,
+                    text=ERROR_INVALID_BILL_NAME
+                )
+
+            bill_id = trans.create_new_bill(text, msg.from_user.id)
+            trans.reset_action(msg.from_user.id, msg.chat_id)
+            return bot.sendMessage(
+                chat_id=msg.chat_id,
+                text=text,
+                reply_markup=self.get_new_bill_keyboard(bill_id)
+            )
+        except Exception as e:
+            return bot.sendMessage(
+                chat_id=msg.chat_id,
+                text=ERROR_SOMETHING_WENT_WRONG
+            )
+
+    def get_new_bill_keyboard(self, bill_id):
+        pass
